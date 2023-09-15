1ba670a01bbde7b84c0602ba8805cbdeac2ed16b
Moshikot3 <moshikot015@gmail.com>
2023-09-15 17:21:25 +0300

    Update pm2.ts

--- server/pm2.ts
+++ server/pm2.ts
@@ -15,7 +15,7 @@ export const getApp = async (name): Promise<IApp> => {
   for (let i = 0; i < list.length; i++) {
     const app = list[i];
 
-    if (app.name != "Whatsapp_forwarder-Bazak") { continue; }
+    if (app.name != "Whatsapp-Forwarder-Plus") { continue; }
 
     apps.push(app);
 
